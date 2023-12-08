b7efc90d4e5d43a6be7c9f0b9e855d90af7945f0
Tiago Mendonça <tiagomendoncaj5@gmail.com>
2023-12-08 16:11:34 -0300

    3.4.1 Declaração de um construtor Account para inicilização de objeto personalizado - pag. 65

--- cap03/Account.java
+++ cap03/Account.java
@@ -1,6 +1,11 @@
 public class Account {
     private String name;
 
+    // o construtor inicializa name com nome do parâmetro
+    public Account(String name){
+        this.name = name;
+    }
+
     public void setName(String name){
         this.name = name;
     }
--- cap03/AccountTest.java
+++ cap03/AccountTest.java
@@ -1,26 +1,11 @@
-import java.util.Scanner;
-
 public class AccountTest {
     public static void main(String[] args) {
-        // Cria um objeto Scanner para obter entrada a partir da janela de comando
-        Scanner input = new Scanner(System.in);
+        // cria dois objetos Account
+        Account account1 = new Account("Jane Green");
+        Account account2 = new Account("John Blue");
 
-        // Cria um objeto Account e o atribui a myAccount
-        Account myAccount = new Account();
-
-        // exibe o valor inicial do nome (null)
-        System.out.printf("Initial name is: %s%n%n", myAccount.getName());
-
-        // Solicita e lê o nome
-        System.out.println("Please enter the name:");
-        String theName = input.nextLine(); // Lê uma linha de texto
-        myAccount.setName(theName); // Insere theName em myAccount
-        System.out.println(); // Gera saída de uma linha em branco
-
-        // exibe o nome armazenado no objeto myAccount
-        System.out.printf("Name in object myAccount is: %n%s%n",
-            myAccount.getName());
-
-        input.close();
+        // exibe o valor inicial de nome para cada Account
+        System.out.printf("account1 name is: %s%n", account1.getName());
+        System.out.printf("account2 name is: %s%n", account2.getName());
     }
 }
